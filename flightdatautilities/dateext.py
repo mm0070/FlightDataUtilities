381d5eaa231538975b9506dcd16378d40c51db4b
Nick Pope <nick.pope@flightdataservices.com>
2016-01-06 12:20:04 +0000

    Migrated library.is_day() from FlightDataAnalyzer.
    Also converted to use math functions from numpy and deduplicated some
    calculations. A little more work should be done to allow this to operate
    on numpy arrays...

--- flightdatautilities/dateext.py
+++ flightdatautilities/dateext.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# vim:et:ft=python:nowrap:sts=4:sw=4:ts=4
 ##############################################################################
 
 '''
@@ -9,6 +10,9 @@ Flight Data Utilities: Date Extensions
 # Imports
 
 
+import __builtin__
+
+import numpy as np
 import operator
 
 from datetime import date, datetime, time, timedelta
@@ -18,11 +22,124 @@ from datetime import date, datetime, time, timedelta
 # Exports
 
 
-__all__ = ['range']
+__all__ = ['is_day', 'range']
 
 
 ##############################################################################
 # Functions
+
+
+def is_day(when, latitude, longitude, twilight='civil'):
+    '''
+    This simple function takes the date, time and location of any point on
+    the earth and return True for day and False for night.
+
+    :param when: Date and time in datetime format
+    :param longitude: Longitude in decimal degrees, east is positive
+    :param latitude: Latitude in decimal degrees, north is positive
+    :param twilight: optional twilight setting. Default='civil', None, 'nautical' or 'astronomical'.
+
+    :raises ValueError if twilight not recognised.
+
+    :returns boolean True = daytime (including twilight), False = nighttime.
+
+    This function is drawn from Jean Meeus' Astronomial Algorithms as
+    implemented by Michel J. Anders. In accordance with his Collective
+    Commons license, the reworked function is being released under the OSL
+    3.0 license by FDS as a part of the POLARIS project.
+
+    For FDM purposes, the actual time of sunrise and sunset is of no
+    interest, so function 12.6 is adapted to give just the day/night
+    decision, with allowance for different, generally recognised, twilight
+    tolerances.
+
+    FAA Regulation FAR 1.1 defines night as: "Night means the time between
+    the end of evening civil twilight and the beginning of morning civil
+    twilight, as published in the American Air Almanac, converted to local
+    time.
+
+    EASA EU OPS 1 Annex 1 item (76) states: 'night' means the period between
+    the end of evening civil twilight and the beginning of morning civil
+    twilight or such other period between sunset and sunrise as may be
+    prescribed by the appropriate authority, as defined by the Member State;
+
+    CAA regulations confusingly define night as 30 minutes either side of
+    sunset and sunrise, then include a civil twilight table in the AIP.
+
+    With these references, it was decided to make civil twilight the default.
+
+    Sources:
+
+    - http://www.esrl.noaa.gov/gmd/grad/solcalc/main.js
+    - http://www.esrl.noaa.gov/gmd/grad/solcalc/calcdetails.html
+    - http://michelanders.blogspot.co.uk/2010/12/calulating-sunrise-and-sunset-in-python.html
+    '''
+    if latitude is np.ma.masked or longitude is np.ma.masked:
+        return np.ma.masked
+
+    day = when.toordinal() - 693594
+    t = when.time()
+    time = (t.hour + t.minute / 60.0 + t.second / 3600.0) / 24.0
+
+    # calculate julian day and century:
+    jd = day + 2415018.5 + time
+    jc = (jd - 2451545.0) / 36525.0
+
+    # siderial time at greenwich
+    gstime = (280.46061837 + 360.98564736629 * (jd - 2451545.0) + (0.0003879331 - jc / 38710000) * jc ** 2) % 360.0
+
+    # geometric mean longitude sun (deg)
+    l0 = (280.46646 + jc * (36000.76983 + jc * 0.0003032)) % 360
+
+    # geometric mean anomaly sun (radians)
+    m = np.radians(357.52911 + jc * (35999.05029 - 0.0001537 * jc))
+
+    # sun equation of center
+    sin1m, sin2m, sin3m = (np.sin(i * m) for i in __builtin__.range(1, 4))
+    c = sin1m * (1.914602 - jc * (0.004817 + 0.000014 * jc)) + sin2m * (0.019993 - 0.000101 * jc) + sin3m * 0.000289
+
+    # calculate elements used in multiple places below:
+    omega = np.radians(125.04 - 1934.136 * jc)
+    latitude = np.radians(latitude)
+
+    # mean obliquity of ecliptic, corrected (radians)
+    seconds = 21.448 - jc * (46.815 + jc * (0.00059 - jc * 0.001813))
+    e0 = 23.0 + (26.0 + seconds / 60.0) / 60.0
+    e = np.radians(e0 + 0.00256 * np.cos(omega))
+
+    # sun true longitude (deg)
+    o = l0 + c
+
+    # sun apparent longitude (radians)
+    lambda_ = np.radians(o - 0.00569 - 0.00478 * np.sin(omega))
+
+    # sun declination (radians)
+    declination = np.arcsin(np.sin(e) * np.sin(lambda_))
+
+    # sun right ascension (deg)
+    rightasc = np.degrees(np.arctan2(np.cos(e) * np.sin(lambda_), np.cos(lambda_)))
+
+    elevation = np.degrees(np.arcsin(
+        np.sin(latitude) * np.sin(declination) +
+        np.cos(latitude) * np.cos(declination) *
+        np.cos(np.radians(gstime + longitude - rightasc))))
+
+    # - Solar diameter gives 0.833 degrees - rim of sun appears before centre of disc.
+    # - For civil twilight, allow 6 degrees.
+    # - For nautical twilight, allow 12 degrees.
+    # - For astronomical twilight, allow 18 degrees.
+    if twilight is None:
+        limit = -0.8333
+    elif twilight == 'civil':
+        limit = -6.0
+    elif twilight == 'nautical':
+        limit = -12.0
+    elif twilight == 'astronomical':
+        limit = -18.0
+    else:
+        raise ValueError('is_day() twilight argument must be one of: civil, nautical, astronomical or None.')
+
+    return elevation > limit  # true = day, false = night
 
 
 def range(start, stop, step=1, field='days'):
@@ -82,7 +199,3 @@ def range(start, stop, step=1, field='days'):
         current += increment
 
     return dates
-
-
-##############################################################################
-# vim:et:ft=python:nowrap:sts=4:sw=4:ts=4
--- tests/dateext_test.py
+++ tests/dateext_test.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# vim:et:ft=python:nowrap:sts=4:sw=4:ts=4
 ##############################################################################
 
 '''
@@ -33,9 +34,70 @@ def setUpModule():
 # Test Cases
 
 
+class TestIsDay(unittest.TestCase):
+    # Solstice times for 2012 at Stonehenge.
+    # Sunset on Wednesday 20th June 2012 is at 2126 hrs (9.26pm BST)
+    # Sunrise on Thursday 21st June 2012 is at 0452 hrs (4.52am BST)
+    def test_sunset(self):
+        self.assertTrue(dateext.is_day(datetime(2012, 6, 20, 20, 25), 51.1789, -1.8264, twilight=None))
+        self.assertFalse(dateext.is_day(datetime(2012, 6, 20, 20, 27), 51.1789, -1.8264, twilight=None))
+
+    def test_sunrise(self):
+        self.assertFalse(dateext.is_day(datetime(2012, 6, 21, 3, 51), 51.1789, -1.8264, twilight=None))
+        self.assertTrue(dateext.is_day(datetime(2012, 6, 21, 3, 53), 51.1789, -1.8264, twilight=None))
+
+    def test_location_west(self):
+        # Sunrise over the Goodyear, Az, office on New Year is 07:33 + 7 hours from UTC
+        self.assertFalse(dateext.is_day(datetime(2013, 1, 1, 14, 32), 33.449291, -112.359015, twilight=None))
+        self.assertTrue(dateext.is_day(datetime(2013, 1, 1, 14, 34), 33.449291, -112.359015, twilight=None))
+
+    def test_location_east(self):
+        # Sunrise over Sydney Harbour Bridge is 3 Jan 2013 05:49 -11 hours from UTC
+        self.assertFalse(dateext.is_day(datetime(2013, 1, 2, 18, 48), -33.85, 151.21, twilight=None))
+        self.assertTrue(dateext.is_day(datetime(2013, 1, 2, 18, 50), -33.85, 151.21, twilight=None))
+
+    def test_midnight_sun(self):
+        # July in Bodo is light all night
+        self.assertTrue(dateext.is_day(datetime(2013, 6, 21, 0, 0),  67.280356,  14.404916, twilight=None))
+
+    def test_midday_gloom(self):
+        # July in Ross Island can be miserable :o(
+        self.assertFalse(dateext.is_day(datetime(2013, 6, 21, 0, 0), -77.52474, 166.960313, twilight=None))
+
+    def test_twilight_libreville(self):
+        # Sunrise on 4 Jun 2012 at Libreville is 06:16. There will be a
+        # partial eclipse on that day visible from Libreville, if you're
+        # interested. Twilight is almost directly proportional to rotation as
+        # this is right on the equator, hence 6 deg = 24 mins.
+        self.assertTrue(dateext.is_day(datetime(2013, 6, 4, 5, 17), 0.454927, 9.411872, twilight=None))
+        self.assertFalse(dateext.is_day(datetime(2013, 6, 4, 5, 15), 0.454927, 9.411872, twilight=None))
+        self.assertTrue(dateext.is_day(datetime(2013, 6, 4, 4, 54), 0.454927, 9.411872, twilight='civil'))
+        self.assertFalse(dateext.is_day(datetime(2013, 6, 4, 4, 52), 0.454927, 9.411872))
+        self.assertTrue(dateext.is_day(datetime(2013, 6, 4, 4, 29), 0.454927, 9.411872, twilight='nautical'))
+        self.assertTrue(dateext.is_day(datetime(2013, 6, 4, 4, 05), 0.454927, 9.411872, twilight='astronomical'))
+
+    def test_uk_aip(self):
+        # These cases are taken from the United Kingdom AIP, page GEN 2.7.1 dated 13 Dec 2012.
+        #Carlisle winter morning
+        lat = 54.0 + 56.0 / 60.0 + 15.0 / 3600.0
+        lon = (2.0 + 48.0 / 60.0 + 33.0 / 3600.0) * -1.0
+        self.assertFalse(dateext.is_day(datetime(2012, 1, 15, 7, 43), lat, lon))
+        self.assertTrue(dateext.is_day(datetime(2012, 1, 15, 7, 45), lat, lon))
+
+        # Lands End summer evening
+        lat = 50.0 + 6.0 / 60.0 + 5.0 / 3600.0
+        lon = (5.0 + 40.0 / 60.0 + 14.0 / 3600.0) * -1.0
+        self.assertFalse(dateext.is_day(datetime(2012, 6, 18, 21, 21), lat, lon))
+        self.assertTrue(dateext.is_day(datetime(2012, 6, 18, 21, 19), lat, lon))
+
+        # Scatsta summer morning
+        lat = 60.0 + 25.0 / 60.0 + 58.0 / 3600.0
+        lon = (1.0 + 17.0 / 60.0 + 46.0 / 3600.0) * -1.0
+        self.assertFalse(dateext.is_day(datetime(2012, 6, 4, 1, 10), lat, lon))
+        self.assertTrue(dateext.is_day(datetime(2012, 6, 4, 1, 12), lat, lon))
+
+
 class TestRange(unittest.TestCase):
-    '''
-    '''
 
     expected = [
         [datetime(2000, 1, x) for x in range(1, 32, 1)],
@@ -46,26 +108,18 @@ class TestRange(unittest.TestCase):
     ]
 
     def test_range__step_float(self):
-        '''
-        '''
         args = (datetime(2000, 1, 1), datetime(2000, 1, 1), 1.5)
         self.assertRaises(TypeError, dateext.range, *args)
 
     def test_range__step_none(self):
-        '''
-        '''
         args = (datetime(2000, 1, 1), datetime(2000, 1, 1), None)
         self.assertRaises(TypeError, dateext.range, *args)
 
     def test_range__step_zero(self):
-        '''
-        '''
         args = (datetime(2000, 1, 1), datetime(2000, 1, 1), 0)
         self.assertRaises(ValueError, dateext.range, *args)
 
     def test_range__step_normal(self):
-        '''
-        '''
         args = (datetime(2000, 1, 1), datetime(2000, 2, 1), 1)
         self.assertEquals(dateext.range(*args), self.expected[0])
         args = (datetime(2000, 1, 31), datetime(1999, 12, 31), -1)
@@ -76,16 +130,12 @@ class TestRange(unittest.TestCase):
         self.assertEquals(dateext.range(*args), self.expected[3])
 
     def test_range__step_reversed(self):
-        '''
-        '''
         args = (datetime(2000, 1, 1), datetime(2000, 2, 1), -1)
         self.assertEquals(dateext.range(*args), [])
         args = (datetime(2000, 1, 31), datetime(1999, 12, 31), 1)
         self.assertEquals(dateext.range(*args), [])
 
     def test_range__valid_field(self):
-        '''
-        '''
         for field in ('seconds', 'minutes', 'hours', 'days', 'weeks'):
             try:
                 args = (datetime(2000, 1, 1), datetime(2000, 2, 1), 1, field)
@@ -94,19 +144,11 @@ class TestRange(unittest.TestCase):
                 self.fail('Unexpected exception raised for field %s' % field)
 
     def test_range__invalid_field(self):
-        '''
-        '''
         for field in ('months', 'years', 'centuries', 'millenia'):
             args = (datetime(2000, 1, 1), datetime(2000, 2, 1), 1, field)
             self.assertRaises(ValueError, dateext.range, *args)
 
     def test_range__date_convert(self):
-        '''
-        '''
         args = (date(2000, 1, 1), date(2000, 1, 5), 12)
         kwargs = {'field': 'hours'}
         self.assertEquals(dateext.range(*args, **kwargs), self.expected[4])
-
-
-##############################################################################
-# vim:et:ft=python:nowrap:sts=4:sw=4:ts=4
