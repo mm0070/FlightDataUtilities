fc9f5c405cf3ec48694f647fd1f0f8aae278f278
Rob Parker <robert.parker@flightdataservices.com>
2016-08-18 09:58:25 +0100

    Added 3+S to conf to lever

--- flightdatautilities/aircrafttables/constants.py
+++ flightdatautilities/aircrafttables/constants.py
@@ -58,6 +58,7 @@ CONF_TO_LEVER = {
     '2':      'Lever 2',
     '2*':     'Lever 3',  # Lever position is 3 when aircraft selects 2*
     '3':      'Lever 3',
+    '3+S':    'Lever 3',  # Lever position is 3 when aircraft selects 3+S
     'Full':   'Lever Full',
 }
 
