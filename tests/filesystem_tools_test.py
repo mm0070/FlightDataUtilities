73962b085e8c2a21864152353d8d0d2ea607d077
Nick Pope <nick.pope@flightdataservices.com>
2017-02-27 10:44:45 +0000

    filesystem tools: fix tests after unification of pretty_size().

--- tests/filesystem_tools_test.py
+++ tests/filesystem_tools_test.py
@@ -6,5 +6,5 @@ import flightdatautilities.filesystem_tools as fst
 class TestPrettySize(unittest.TestCase):
 
     def test_pretty_size(self):
-        self.assertEqual(fst.pretty_size(213458923), '203.57MB')
-        self.assertEqual(fst.pretty_size(1234), '1.21KB')
+        self.assertEqual(fst.pretty_size(213458923), '203.6MB')
+        self.assertEqual(fst.pretty_size(1234), '1.2KB')
