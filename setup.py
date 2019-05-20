dddd9760de4d68a6b726a62b5e995a11ea40d1f5
Glen Kirkup <glen.kirkup@flightdataservices.com>
2019-05-20 13:30:14 +0100

    add cython .pxd header file for source distribution

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import numpy as np
 
 setup(
     use_scm_version={'fallback_version': '19.0.0'},
+    data_files=[('', ['flightdatautilities/array.pxd'])],
     ext_modules=[
         Extension(
             'flightdatautilities.array', ['flightdatautilities/array.pyx'],
