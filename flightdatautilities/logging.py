da83fd9042b23a9c1dbf55eb64b95bef39cd468f
Nick Pope <nick.pope@flightdataservices.com>
2019-08-28 12:31:50 +0100

    logging: add a custom formatter for console logging.
    This adds support for outputting the structured logging information
    passed to extra, as well as color and emoji support!

--- flightdatautilities/logging.py
+++ flightdatautilities/logging.py
@@ -1,7 +1,9 @@
 import datetime
 import logging
+import os
 import queue
 import socket
+import sys
 import threading
 
 import elasticsearch
@@ -25,6 +27,57 @@ class Serializer(elasticsearch.serializer.JSONSerializer):
             return super().default(data)
         except TypeError:
             return str(data)
+
+
+class ConsoleFormatter(logging.Formatter):
+    _color_map = {
+        logging.CRITICAL: '1;31',
+        logging.ERROR: '31',
+        logging.WARNING: '33',
+        logging.INFO: '97',
+        logging.DEBUG: '37',
+    }
+    _emoji_map = {
+        logging.CRITICAL: '\U0001f525',
+        logging.ERROR: '\u274c',
+        logging.WARNING: '\u26a0\ufe0f\u0020',
+        logging.INFO: '\u2139\ufe0f\u0020',
+        logging.DEBUG: '\U0001f41b',
+    }
+
+    @property
+    def _check_support(self):
+        supported = sys.platform != 'win32' or 'ANSICON' in os.environ
+        isatty = hasattr(sys.stdout, 'isatty') and sys.stdout.isatty()
+        return supported and isatty
+
+    def __init__(self, *args, **kwargs):
+        self._color = kwargs.pop('color', self._check_support)
+        self._emoji = kwargs.pop('emoji', self._check_support)
+        self._exclude = {'asctime', 'message'} | logging.makeLogRecord({}).__dict__.keys()
+        super().__init__(*args, **kwargs)
+
+    def format(self, record):
+        if self._emoji:
+            record.levelname = self._emoji_map.get(record.levelno, '\u2754')
+        value = super().format(record)
+        color = self._color and self._color_map.get(record.levelno)
+        return f'\x1b[{color}m{value}\x1b[0m' if color else value
+
+    def formatMessage(self, record):
+        value = super().formatMessage(record)
+        extra = {k: v for k, v in record.__dict__.items() if k not in self._exclude}
+        if extra:
+            extra = f'\x1b[2;3;37m{extra}\x1b[0m' if self._color else extra
+        return value + ' ' + extra if extra else value
+
+    def formatStack(self, value):
+        value = super().formatStack(value)
+        return f'\x1b[2;3;37m{value}\x1b[0m' if self._color else value
+
+    def formatException(self, value):
+        value = super().formatException(value)
+        return f'\x1b[2;3;37m{value}\x1b[0m' if self._color else value
 
 
 class ElasticsearchHandler(logging.Handler):
