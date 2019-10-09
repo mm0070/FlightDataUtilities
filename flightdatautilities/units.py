fc5dcb77895bcff10c8660a8d5624fba839776f2
Michal Mildner <michal.mildner@flightdataservices.com>
2019-10-09 12:40:07 +0100

    use numpy to convert degrees to radians

--- flightdatautilities/units.py
+++ flightdatautilities/units.py
@@ -19,8 +19,9 @@ from __future__ import unicode_literals
 
 import decimal
 import math
-import six
 
+import six
+import numpy as np
 
 ##############################################################################
 # Constants
@@ -492,10 +493,10 @@ CONVERSION_MULTIPLIERS = {
 CONVERSION_FUNCTIONS = {
     # Angles:
     DEGREE: {
-        RADIAN: math.radians,
+        RADIAN: np.radians,
     },
     RADIAN: {
-        DEGREE: math.degrees,
+        DEGREE: np.degrees,
     },
     # Temperature:
     CELSIUS: {
