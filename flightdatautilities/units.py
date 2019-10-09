43cf57d70c6a208181973ea937ecbaed37f13f10
Michal Mildner <michal.mildner@flightdataservices.com>
2019-10-09 17:12:42 +0100

    Feature/mm/radians (#16)
    * added radians per second
    
    * use numpy to convert degrees to radians
    
    * removed unused imports

--- flightdatautilities/units.py
+++ flightdatautilities/units.py
@@ -14,8 +14,9 @@ The following links are useful resources:
 
 
 import decimal
-import math
 
+import six
+import numpy as np
 
 ##############################################################################
 # Constants
@@ -487,10 +488,10 @@ CONVERSION_MULTIPLIERS = {
 CONVERSION_FUNCTIONS = {
     # Angles:
     DEGREE: {
-        RADIAN: math.radians,
+        RADIAN: np.radians,
     },
     RADIAN: {
-        DEGREE: math.degrees,
+        DEGREE: np.degrees,
     },
     # Temperature:
     CELSIUS: {
