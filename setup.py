a9c8a76d5edecb163be883fc839328da71605ee6
Glen Kirkup <glen.kirkup@flightdataservices.com>
2019-10-15 23:41:11 +0100

    automates setup.py extensions

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
+from pathlib import Path
 from setuptools import setup, Extension
+
 import numpy as np
 
 
 setup(
     use_scm_version={'fallback_version': '19.0.0'},
-    data_files=[('', ['flightdatautilities/array.pxd'])],
+    data_files=[('', [path.as_posix() for path in Path().glob('**/*.pxd')])],
     ext_modules=[
-        Extension(
-            'flightdatautilities.array', ['flightdatautilities/array.pyx'],
-            include_dirs=[np.get_include()],
-        ),
-    ],
+        Extension('.'.join(path.parent.parts + (path.stem,)), [path.as_posix()],
+                  define_macros=[('CYTHON_TRACE_NOGIL', '1'), ('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
+                  include_dirs=[np.get_include()]) for path in Path().glob('**/*.pyx')],
 )
