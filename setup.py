ccbaf684912e479b3fc8cd8c6023300d0a5e09ae
Nick Pope <nick.pope@flightdataservices.com>
2019-11-29 15:28:15 +0000

    cleanup: modernised packaging, metadata, etc.

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-import itertools
+from pathlib import Path
 import sys
 
-from pathlib import Path
-from setuptools import setup, Extension
-
 import numpy as np
+import setuptools
 
 
 filenames = set()
@@ -19,11 +17,11 @@ if index:
             index += 1
 
 
-setup(
+setuptools.setup(
     use_scm_version={'fallback_version': '19.0.0'},
     data_files=[('', [path.as_posix() for path in Path().glob('**/*.pxd')])],
-    ext_modules=[Extension('.'.join(path.parent.parts + (path.stem,)), [path.as_posix()],
-                           define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
-                           include_dirs=[np.get_include()]) for path in Path().glob('**/*.pyx')
+    ext_modules=[setuptools.Extension('.'.join(path.parent.parts + (path.stem,)), [path.as_posix()],
+                                      define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
+                                      include_dirs=[np.get_include()]) for path in Path().glob('**/*.pyx')
                  if not filenames or path.name in filenames],
 )
