7baa9416346fe8c5b5d1e9e1ee2b97f644e2f2d7
Glen Kirkup <glen.kirkup@flightdataservices.com>
2020-02-05 10:31:43 +0000

    removes setup.py cython hack as unnecessary compilation is now being skipped properly

--- setup.py
+++ setup.py
@@ -1,27 +1,12 @@
 from pathlib import Path
-import sys
 
 import numpy as np
 import setuptools
 
-
-filenames = set()
-index = sys.argv.index('build_ext')
-if index:
-    index += 1
-    while index <= len(sys.argv) - 2:
-        if sys.argv[index] == '--filename':
-            filenames.add(sys.argv[index + 1])
-            sys.argv[index:index + 2] = []
-        else:
-            index += 1
-
-
 setuptools.setup(
-    use_scm_version={'fallback_version': '19.0.0'},
     data_files=[('', [path.as_posix() for path in Path().glob('**/*.pxd')])],
     ext_modules=[setuptools.Extension('.'.join(path.parent.parts + (path.stem,)), [path.as_posix()],
                                       define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
-                                      include_dirs=[np.get_include()]) for path in Path().glob('**/*.pyx')
-                 if not filenames or path.name in filenames],
+                                      include_dirs=[np.get_include()]) for path in Path().glob('**/*.pyx')],
+    use_scm_version={'fallback_version': '19.0.0'},
 )
